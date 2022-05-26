72805a98063d6b625021462f7cc243a3c03351cf
EvanQiuu <90792438+EvanQiuu@users.noreply.github.com>
2022-05-26 13:32:00 -0400

    Revert "ff"
    This reverts commit 53610feef0968754465f1f5f9a7ffc2ebf78ca15.

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,18 @@
 import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
+<div class="toggle-container">
+  <button
+    class="theme-btn light"
+    onclick="setTheme('light')"
+    title="Light mode"
+  ></button>
+  <button
+    class="theme-btn dark"
+    onclick="setTheme('dark')"
+    title="Dark mode"
+  ></button>
+</div>;
